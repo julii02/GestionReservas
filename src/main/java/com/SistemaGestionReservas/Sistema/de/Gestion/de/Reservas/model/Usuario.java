7acdc0903baf5ee2eb9933ce9cefb2ec8cd74420
julii02 <juliangonzalez6102@gmail.com>
2024-09-21 14:31:24 -0300

    actualizar usuario

--- src/main/java/com/SistemaGestionReservas/Sistema/de/Gestion/de/Reservas/model/Usuario.java
+++ src/main/java/com/SistemaGestionReservas/Sistema/de/Gestion/de/Reservas/model/Usuario.java
@@ -4,4 +4,5 @@ package com.SistemaGestionReservas.Sistema.de.Gestion.de.Reservas.model;
 public class Usuario {
     private long idUsuario;
     private String nombre;
+    private String apellido;
 }
