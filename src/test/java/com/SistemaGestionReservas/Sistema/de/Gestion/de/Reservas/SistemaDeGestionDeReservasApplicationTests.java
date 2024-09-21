67fc59cbd970ae28fff5cbf0530e8ca3773b7e11
julii02 <juliangonzalez6102@gmail.com>
2024-09-21 14:16:07 -0300

    Segundo Commit

---
+++ src/test/java/com/SistemaGestionReservas/Sistema/de/Gestion/de/Reservas/SistemaDeGestionDeReservasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SistemaGestionReservas.Sistema.de.Gestion.de.Reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestionDeReservasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
