fe8839dbb41587c83cc262862684238f7a390a04
julii02 <juliangonzalez6102@gmail.com>
2024-09-21 14:07:15 -0300

    Primer Commit

---
+++ src/main/java/com/SistemaGestionReservas/Sistema/de/Gestion/de/Reservas/SistemaDeGestionDeReservasApplication.java
@@ -0,0 +1,13 @@
+package com.SistemaGestionReservas.Sistema.de.Gestion.de.Reservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestionDeReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestionDeReservasApplication.class, args);
+	}
+
+}
