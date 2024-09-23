d92cfe4eb79b6107b7f53a5ccbdfbc70def0f53e
julii02 <juliangonzalez6102@gmail.com>
2024-09-22 23:05:25 -0300

    entidad usuario creada

--- src/main/java/com/SistemaGestionReservas/Sistema/de/Gestion/de/Reservas/model/Usuario.java
+++ src/main/java/com/SistemaGestionReservas/Sistema/de/Gestion/de/Reservas/model/Usuario.java
@@ -1,8 +1,35 @@
 
 package com.SistemaGestionReservas.Sistema.de.Gestion.de.Reservas.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long idUsuario;
     private String nombre;
     private String apellido;
+    private String email;
+
+    public Usuario() {
+    }
+
+    public Usuario(long idUsuario, String nombre, String apellido, String email) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+    
+    
 }
+
+    
+
